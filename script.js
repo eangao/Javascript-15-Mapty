53d71ed5f2b5eb5523f5af68b370960008f5348b
elmar <elmarcera@gmail.com>
2023-12-20 14:24:30 +0800

    Displaying a Map Using Leaflet Library

--- script.js
+++ script.js
@@ -66,6 +66,22 @@ if (navigator.geolocation) {
       const { longitude } = position.coords;
       console.log(latitude, longitude);
       console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      //https://leafletjs.com/index.html
+      //map is the id in the html
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
 
     // and this is the error one.
@@ -74,3 +90,82 @@ if (navigator.geolocation) {
     }
   );
 }
+
+/////////////////////////////////////////////////////////////////////
+// Displaying a Map Using Leaflet
+/////////////////////////////////////////////////////////////////////
+// we gonna learn how to display
+// a map using a third party library called Leaflet.
+// So to start let's quickly Google Leaflet
+// and probably the first result will already
+// be for this library.
+
+// So you'll see that Leaflet
+// is an open source JavaScript library
+// for mobile-friendly interactive maps.
+// So essentially this is a library
+// that some other developers wrote
+// and that we can now include
+// for free our own code and use it.
+
+// And in this case,
+// we can use it to display in maps
+// and whenever we use a third-party library,
+// the first thing to do is to basically include it
+// in our site.
+
+// And so let's come here to the Downloads Page.
+// And so here we can download Leaflet
+// to our computer if we wanted
+// or we can also use a hosted version of Leaflet.
+
+// And this basically means that we can use
+// a version of this library that is already hosted
+// by someone else.
+// So in this case,
+// it is a CDN which is a Content Delivery Network.
+// And so it has this URL here.
+// So that stands for unpackaged.com.
+
+// And then here we have the Leaflet Library.
+// So actually we will need a CSS file
+// and we will also need a JavaScript file.
+// Now, there are also a bit more elegant ways
+// of including JavaScript,
+// for example using a package manager like NPM.
+
+// And then all we would have to do
+// is NPM install Leaflet into our application.
+// And actually this is how we will do it in the future.
+// So in one of the next sections,
+// we will actually learn how to do this
+// using the NPM package manager,
+// but for now probably the easiest way
+// to get started is to simply include
+// a hosted version that is on a CDN.
+
+// And so let's grab this code here
+// and paste it in our HTMLs' hat.
+// So right here and to before our own script
+// and that's very important.
+// All right, so this here is our script
+// and of course it is in our script
+// where we will then use the Leaflet Library.
+
+// And so by the time that our script loads,
+// the browser must already have downloaded
+// the Leaflet Library because otherwise
+// our code will not be able to work without library.
+
+// And that makes sense, right?
+// Now here we also want to specify actually
+// the Differ Attribute.
+// So as we learned previously,
+// we should never put any JavaScript
+// in the header without any of the Differ or Async Attributes.
+// And since the order matters here,
+// remember, we must then use the Differ Attribute.
+// And so this is actually a perfect example
+// for when we need to use Differ because again
+// here the order in which the scripts are downloaded
+// is actually very important.
